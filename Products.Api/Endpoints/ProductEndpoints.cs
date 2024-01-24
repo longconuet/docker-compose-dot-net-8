d4a63b34c144f47ac73fd973b57bb34fef8a378a
longnt-wl <long@world-link-system.com>
2024-01-24 17:36:51 +0700

    initPrj

---
+++ Products.Api/Endpoints/ProductEndpoints.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Products.Api.Database;
+using Products.Api.Entities;
+using Products.Api.Requests;
+
+namespace Products.Api.Endpoints
+{
+    public static class ProductEndpoints
+    {
+        public static void MapProductEndpoints(this IEndpointRouteBuilder app)
+        {
+            app.MapGet("products", async (ApplicationDbContext context) =>
+            {
+                var products = await context.Products.ToListAsync();
+
+                return Results.Ok(products);
+            });
+
+            app.MapPost("products", async (CreateProductRequest request, ApplicationDbContext context) =>
+            {
+                var product = new Product
+                {
+                    Id = Guid.NewGuid(),
+                    Name = request.Name,
+                    Price = request.Price
+                };
+
+                context.Add(product);
+                await context.SaveChangesAsync();
+
+                return Results.Ok(product);
+            });
+        }
+    }
+}
---
+++ Products.Api/Requests/CreateProductRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Products.Api.Requests
+{
+    public class CreateProductRequest
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
